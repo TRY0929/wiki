a309adbc5a996275c05a4bae453ff856864d3a09
TRY0929 <trytry0929@gmail.com>
2024-01-20 17:09:13 +0900

    add spring AOP

--- src/main/java/com/puiken/wiki/filter/LogFilter.java
+++ src/main/java/com/puiken/wiki/filter/LogFilter.java
@@ -1,33 +1,33 @@
- package com.puiken.wiki.filter;
-
- import jakarta.servlet.*;
- import jakarta.servlet.http.HttpServletRequest;
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- import org.springframework.stereotype.Component;
-
- import java.io.IOException;
-
- @Component
- public class LogFilter implements Filter {
-
-     private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-     @Override
-     public void init(FilterConfig filterConfig) throws ServletException {
-
-     }
-
-     @Override
-     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-         // 打印请求信息
-         HttpServletRequest request = (HttpServletRequest) servletRequest;
-         LOG.info("------------- LogFilter begin -------------");
-         LOG.info("Request address: {} {}", request.getRequestURL().toString(), request.getMethod());
-         LOG.info("Remote address: {}", request.getRemoteAddr());
-
-         long startTime = System.currentTimeMillis();
-         filterChain.doFilter(servletRequest, servletResponse);
-         LOG.info("------------- LogFilter end time：{} ms -------------", System.currentTimeMillis() - startTime);
-     }
- }
+// package com.puiken.wiki.filter;
+//
+// import jakarta.servlet.*;
+// import jakarta.servlet.http.HttpServletRequest;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Component;
+//
+// import java.io.IOException;
+//
+// @Component
+// public class LogFilter implements Filter {
+//
+//     private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//     @Override
+//     public void init(FilterConfig filterConfig) throws ServletException {
+//
+//     }
+//
+//     @Override
+//     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//         // 打印请求信息
+//         HttpServletRequest request = (HttpServletRequest) servletRequest;
+//         LOG.info("------------- LogFilter begin -------------");
+//         LOG.info("Request address: {} {}", request.getRequestURL().toString(), request.getMethod());
+//         LOG.info("Remote address: {}", request.getRemoteAddr());
+//
+//         long startTime = System.currentTimeMillis();
+//         filterChain.doFilter(servletRequest, servletResponse);
+//         LOG.info("------------- LogFilter end time：{} ms -------------", System.currentTimeMillis() - startTime);
+//     }
+// }
